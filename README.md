5b2f2399ffd9a85fbabe587a23d851454971e164
SotwareByRedline <krajczar.david41@gmail.com>
2021-01-29 20:01:15 +0100

    Added Save As button and some more Python blocks

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Syntax Blocks
 
-The code editor fighting language syntax barriers.
+The code editor fighting language syntax barriers. Now with very experimental editing.
+
+Automatic tab inserts don't yet work.
--- sblocks.py
+++ sblocks.py
@@ -8,11 +8,28 @@ mw.title("SyntaxBlocks alpha")
 
 selected_lang = "Python"
 
+# Language blocks database
 languages = {
     "Python": {
-        "functions": {
-            "Print to console": "print()",
+        "Basics": {
             "Text": "\"\"",
+            "Add": "+",
+            "Subtract": "-",
+            "Multiply": "*",
+            "Divide": "/",
+            "Int divide": "//",
+            "Power": "^"
+        },
+        "Blocks": {
+            "If": "if :\n",
+            "If else": "if :\n\nelse:\n",
+            "For loop with simple counter": "for  in range()",
+            "For loop with advanced counter (start, stop, step)": "for  in range( , , )",
+            "While loop": "while :\n"
+        },
+        "Functions": {
+            "Print to console": "print()",
+            "Evaluate statement an return result": "eval()",
         }
     }
 }  # List of programming languages available in the app
@@ -37,11 +54,23 @@ def save_file():
 
     file_out.close()
 
+def save_as():
+    global file_path
+    file_path = filedialog.askopenfilename()
+
+    file_out = open(file_path, "w")
+
+    for line in code_editor.get("1.0", "end-1c"):
+        print(line, file=file_out, end="")
+
+    file_out.close()
+
 
 # Configure menus
 file_menu = Menu(mw)
 file_menu.add_command(label="Open", command=open_file)
 file_menu.add_command(label="Save", command=save_file)
+file_menu.add_command(label="Save as", command=save_as)
 
 menubar = Menu(mw)
 mw.config(menu=menubar)
@@ -52,7 +81,9 @@ menubar.add_separator()
 for unit in languages[selected_lang].keys():
     exec(unit + "_menu = Menu(mw)")  # Create a block group
     for block in languages[selected_lang][unit].keys():
-        exec(unit + "_menu.add_command(label=block, command=lambda: code_editor.insert(INSERT, languages[selected_lang][\"" + unit + "\"][\"" + block +"\"]))") # Add a block to a category
+        exec(
+            unit + "_menu.add_command(label=block, command=lambda: code_editor.insert(INSERT, languages["
+                   "selected_lang][\"" + unit + "\"][\"" + block + "\"]))")  # Add a block to a category
 
     exec("menubar.add_cascade(label=\"{0}\", menu={1}_menu)".format(unit, unit))  # Add current block group to menu
 
